3cff917892c20f5cdae4f666250d716ecd62abe1
Saksham-garg <sakshamgarg142@hotmail.com>
2024-09-14 03:48:29 +0530

    added files

---
+++ developerportfolio/schemaTypes/index.js
@@ -0,0 +1,18 @@
+import about from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import work from './works'
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experiences,
+    skills,
+    testimonials,
+    workExperience,
+    work
+]
